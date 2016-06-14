0608b351f6f11e2ef79acd43d3835f38122133d2
Eazhilarasi <eazhil@manivannan.org>
2016-06-14 13:03:57 +0100

    displays the home time line with tweet and name of the person who has tweeted

---
+++ ios_client/Lovelace/Lovelace/Tweet.swift
@@ -0,0 +1,21 @@
+//
+//  Tweet.swift
+//  Lovelace
+//
+//  Created by Eazhilarasi Manivannan on 14/06/2016.
+//  Copyright © 2016 lovelaceTeam. All rights reserved.
+//
+
+import Foundation
+
+public class Tweet
+{
+    var tweet: String
+    var userName: String
+    
+    init(tweet: String, userName: String) {
+        self.tweet = tweet
+        self.userName = userName
+    }
+    
+}
--- ios_client/Lovelace/Lovelace/ViewController.swift
+++ ios_client/Lovelace/Lovelace/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tweetsListTableView: UITableView!
+    var tweetList = [Tweet]()
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
@@ -22,16 +25,40 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func buttonClicked(sender: UIButton) {
+    @IBAction func getHomeTimeline(sender: AnyObject) {
         APIManager.getHomeLine { homeLineTweets in
             for (_, tweet) in homeLineTweets {
-                
+                let tweetText = tweet["text"].stringValue
+                let userName = tweet["user"]["name"].stringValue
+                let tweetObj = Tweet(tweet: tweetText, userName: userName)
+                self.tweetList.append(tweetObj)
                 print( tweet["text"] )
             }
+            self.tweetsListTableView.reloadData()
         }
+        //dispatch_async(dispatch_get_main_queue()){
+        
+            
+       // }
     }
-
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) ->
+        Int
+    {
+        return self.tweetList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let tweetCell = tableView.dequeueReusableCellWithIdentifier("tweetPrototypeCell", forIndexPath: indexPath)
+        
+        // Even if the prototype cell has not visible label in designer, it has the textLabel label by default
+        tweetCell.textLabel?.text = tweetList[indexPath.row].userName
+        tweetCell.detailTextLabel?.text = tweetList[indexPath.row].tweet
+        
+        return tweetCell
+    }
+    
+    
 }
 
