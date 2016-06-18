5f5ddcf2262df5df79643269821c12058cf8f6d5
Eazhilarasi <eazhilarasi.manivannan@ucdconnect.ie>
2016-06-18 15:30:06 +0100

    iOS- Home TimeLine of User (#41)
    * displays the home time line with tweet and name of the person who has tweeted
    
    * Added image to the tweet list and fixed issue with image showing up in the list
    
    * Added constraits to the table view in Home scene

---
+++ ios_client/Lovelace/Lovelace/TweetViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetViewCell.swift
+//  Lovelace
+//
+//  Created by Eazhilarasi Manivannan on 15/06/2016.
+//  Copyright © 2016 lovelaceTeam. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewCell: UITableViewCell {
+    
+
+    @IBOutlet weak var tweetUserImage: UIImageView!
+    
+    
+    @IBOutlet weak var tweetUserName: UILabel!
+    
+  
+    @IBOutlet weak var tweetDateTime: UILabel!
+    
+    
+    @IBOutlet weak var tweetUserDisplayName: UILabel!
+    
+    
+    @IBOutlet weak var tweetText: UILabel!
+    
+}
