945f398ebf655bc8d0b1dc8f55973139e1fd31c0
Xinqi Li <xinqi.li@ucdconnect.ie>
2016-06-19 22:11:23 +0100

    API: Integrated Basic Recommender System with Flask (v 1.0)

--- api/Lovelace/Lovelace.py
+++ api/Lovelace/Lovelace.py
@@ -4,73 +4,76 @@ import tweepy
 import json
 from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
+from recommender import Recommender
 
 app = Flask(__name__)
 api = Api(app)
 
 consumer_key    = "BbTvs8T7CZguiloHMIVeRdKUO"
 consumer_secret = "Ji9JyeCKRrY9DUhE0ry0wWpYcVxJMHyOheqGc62VJOB4UsBXZy"
+#consumer_key = 'WtxItBWIIw35Ei1tQ4Zrmkybk'
+#consumer_secret = '7KV0Mmg1P7qrIrYCeeRB5V1nKrVRK0r3PQiy7RwNWYTCDxNevH'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
 class UserTimeline(Resource):
     def get(self):
-        
+
         # access_token = request.headers['oauth_token']
         # access_token_secret = request.headers['oauth_token_secret']
         access_token = request.args.get('oauth_token')
         access_token_secret = request.args.get('oauth_token_secret')
-        
+
         auth.set_access_token(access_token, access_token_secret)
         api = tweepy.API(auth)
-        
+
         #fetch 100 tweets from user's timeline
         user_tweets = [tweet._json for tweet in tweepy.Cursor(api.user_timeline, count=200).items(100)]
         tweets_texts = [tweet['text'] for tweet in user_tweets]
-        
+
         #generate a list of stop words
         stop_words = [word for word in CountVectorizer(stop_words='english').get_stop_words()]
         stop_words.append('rt')
         stop_words.append('https')
-        
+
         #tokenize words, convert to lowercase, filter out stop words
         tokenize = CountVectorizer().build_tokenizer()
         words = [word.lower() for sentence in tweets_texts
                                 for word in tokenize(sentence)
                                     if len(word.lower()) > 1 and word.lower() not in stop_words]
-        
+
         #count term frequency
         c = Counter(words)
         word_counts = c.most_common()
         word_counts[:5]
-        
+
         #search for 100 tweets from the account that the user is not following
         unfollowed_tweets = [tweet._json['text'] for tweet in tweepy.Cursor(api.search, q=word_counts[0][0], count=200,lang='en').items(100)]
-        
+
         #return results
         results = {}
         results['user_tweets'] = user_tweets
         results['word_count'] = word_counts[:5]
         results['unfollowed_tweets'] = unfollowed_tweets
-                 
+
         return jsonify(results)
 
 
 
 class HomeTimeline(Resource):
     def get(self):
-        
+
         # access_token = request.headers['oauth_token']
         # access_token_secret = request.headers['oauth_token_secret']
         access_token = request.args.get('oauth_token')
         access_token_secret = request.args.get('oauth_token_secret')
-        
+
         auth.set_access_token(access_token, access_token_secret)
         api = tweepy.API(auth)
-        
+
         #fetch 100 tweets from user
         home_tweets = [tweet._json for tweet in tweepy.Cursor(api.home_timeline, count=200).items(100)]
-        
+
         return jsonify(home_tweets)
 
 class Tweets(Resource):
@@ -79,7 +82,7 @@ class Tweets(Resource):
         access_token = request.args.get('oauth_token')
         access_token_secret = request.args.get('oauth_token_secret')
         page = request.args.get('page')
-        
+
         auth.set_access_token(access_token, access_token_secret)
         api = tweepy.API(auth)
 
@@ -87,6 +90,34 @@ class Tweets(Resource):
         tweets = [tweet._json for tweet in api.home_timeline(page=page)]
 
         return jsonify(tweets)
+
+#The recommend system part
+class RecommendTweets(Resource):
+    def get(self):
+
+        access_token = request.args.get('oauth_token')
+        access_token_secret = request.args.get('oauth_token_secret')
+        page = request.args.get('page')
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        
+        #get user's information
+        user = api.me()
+        
+        #fetch tweets of user's home timeline
+        home_tweets = [tweet._json for tweet in tweepy.Cursor(api.home_timeline, count=20, page=page).items(20)]
+        
+        #fetch tweets of user's own time line
+        users_tweets = [tweet._json for tweet in tweepy.Cursor(api.user_timeline, count=50).items(50)]
+        
+        #filter out the tweets that the user tweeted from the home timeline
+        followed_tweets = [tweet for tweet in home_tweets if tweet['user']['screen_name'] != user._json['screen_name']]
+        
+        #give the user timeline and home timeline to the recommender system to make recommendation
+        recommender_object = Recommender(followed_tweets, users_tweets)
+        recommended_tweets = recommender_object.generate(3, None, None)
+        
+        return jsonify(recommended_tweets)
 
 
 class IOSAppRedirectHelper(Resource):
@@ -109,6 +140,7 @@ api.add_resource(IOSAppRedirectHelper, '/oauth-callback')
 api.add_resource(UserTimeline, '/user_timeline')
 api.add_resource(HomeTimeline, '/home_timeline')
 api.add_resource(Tweets, '/tweets')
+api.add_resource(RecommendTweets, '/recommend')
 
 
 if __name__ == '__main__':
---
+++ api/Lovelace/recommender.py
@@ -0,0 +1,69 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+class Recommender:
+    # def __init__(self, followed_tweets):
+    #     self.followed_tweets = followed_tweets
+    #     self.vectorizer = CountVectorizer()
+    #
+    # def generate(self, user_tweets, number_of_recommendations, followed_accounts, how_many_days_ago):
+    #     self.vectorizer.fit_transform(user_tweets)#user tweets
+    #     results = sorted(self.followed_tweets, key=self.count_bag, reverse=True)
+    #
+    #     return results
+    #
+    # def count_bag(self, followed_tweets):
+    #     count = 0
+    #     terms = self.vectorizer.get_feature_names()#user tweets
+    #     words = followed_tweets.split()
+    #
+    #     for word in words:
+    #         if word in terms:
+    #             count += 1
+    #
+    #     return count
+
+    def __init__(self, followed_tweets, users_tweets):
+        self.followed_tweets = followed_tweets
+        self.users_tweets = users_tweets
+        self.vectorizer = CountVectorizer()
+        self.counts = []
+
+    def generate(self, number_of_recommendations, followed_accounts, how_many_days_ago):
+
+        #generate a count list
+        counts = [self.count_bag(tweet) for tweet in self.followed_tweets]
+        
+        #return sorted home timeline and sorted list of count
+        return {"recommended_tweets":sorted(self.followed_tweets, key=self.count_bag, reverse=True), "counts":sorted(counts, reverse=True)}
+
+    def count_bag(self, item):
+
+        # generate a list of stop words
+        stop_words = [word for word in CountVectorizer(stop_words='english').get_stop_words()]
+        stop_words.append('rt')#retweets
+        stop_words.append('https')#urls
+
+        # fetch the text of user's own timeline
+        users_tweets_text = [tweet['text'] for tweet in self.users_tweets]
+
+        # for user's own timeline, tokenize words, convert to lowercase, filter out stop words
+        #generate a list of terms of the user's own timeline
+        tokenize = self.vectorizer.build_tokenizer()
+        terms = [word.lower() for sentence in users_tweets_text
+                 for word in tokenize(sentence)
+                 if len(word.lower()) > 1 and word.lower() not in stop_words]
+                 
+        
+        count = 0
+        #for each tweet in home timeline, tokenize words, convert to lowercase, filter out stop words
+        words = [word.lower() for word in tokenize(item['text'])
+                 if len(word.lower()) > 1 and word.lower() not in stop_words]
+
+        #the for each word in words, if it exists in the terms of user's own timeline, the increase count by one
+        #at last
+        for word in words:
+            if word in terms:
+                count += 1
+
+        return count
