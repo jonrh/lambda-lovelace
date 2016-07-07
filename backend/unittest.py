4d4130bd0e75bfd841681afc72affe3e27d92a69
Jón Rúnar Helgason <jonrh@jonrh.is>
2016-07-07 18:04:17 +0100

    Backend: Switched out Python unit test for Nose unit test

--- backend/unittest.py
+++ backend/unittest.py
@@ -1,9 +1,2 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-
-    def test_one_plus_one(self):
-        self.assertEqual(1 + 1, 2)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_one_plus_one():
+    assert 1 + 1 == 2
