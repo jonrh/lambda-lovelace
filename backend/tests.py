1663d34367af661ea94f7fe532e5d7a67f37bca9
Jón Rúnar Helgason <jonrh@jonrh.is>
2016-08-01 20:47:53 +0100

    Backend: Fixing broken tests

--- backend/tests.py
+++ backend/tests.py
@@ -9,7 +9,12 @@ def test_one_plus_one():
 
 
 def test_recommender_backend():
-    urllib2.urlopen("http://backend-testing/recommend?oauth_token=735845050458984448-oLfMuBPTyCnOw2yPEU0MdJ65YxX1BwU&oauth_token_secret=veyfudJQz0TMY5S8K6VwzPfteQFVPOSdU1yWwq5fWtZHC&page=1")
+    url = """http://backend-testing/recommend
+        ?oauth_token=735845050458984448-oLfMuBPTyCnOw2yPEU0MdJ65YxX1BwU
+        &oauth_token_secret=veyfudJQz0TMY5S8K6VwzPfteQFVPOSdU1yWwq5fWtZHC
+        &page=1"""
+
+    urllib2.urlopen(url).read()
 
 
 def test_hello_endpoint():
