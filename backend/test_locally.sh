003f55e1465ad820eb31c99c63819335204fa375
Jón Rúnar Helgason <jonrh@jonrh.is>
2016-08-02 17:11:10 +0100

    Backend: Initial draft of a shell script to test locally

---
+++ backend/test_locally.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# =============================================================================
+#
+#
+# Author: Jón Rúnar Helgason, jonrh, 2016
+# =============================================================================
+
+cd backend/
+docker build -t "test_image" .
+
+docker run -d --name="test_container" --rm "test_image"
+
+sleep 5s
+
+docker run --name "test-unittests" --link "backend-testing" --rm "test_image" nosetests tests.py
