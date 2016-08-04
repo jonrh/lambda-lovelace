bfe1a78f91850b9a46aa43d38596c9ce267e9c14
Jón Rúnar Helgason <jonrh@jonrh.is>
2016-08-04 12:29:57 +0100

    Backend: Forgot to switch to the production IP and port

--- backend/Lovelace.py
+++ backend/Lovelace.py
@@ -292,5 +292,5 @@ api.add_resource(EvaluationResult, '/evaluationResult')
 api.add_resource(UserProfile, '/userProfile')
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=80)  # Production
-    app.run(host="127.0.0.1", port=5000)  # Local debugging
+    app.run(host="0.0.0.0", port=80)  # Production
+    # app.run(host="127.0.0.1", port=5000)  # Local debugging
