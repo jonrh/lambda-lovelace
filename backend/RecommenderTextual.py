fb149f8e7e48db998bffc46d8dbae4fbe1a4e5e3
marclaffan@gmail.com <marclaffan@gmail.com>
2016-08-06 14:49:20 +0100

    Integrated feedback for authors, dislikes and likes

--- backend/RecommenderTextual.py
+++ backend/RecommenderTextual.py
@@ -21,7 +21,7 @@ class RecommenderTextual:
         # to use when creating their term frequency document.
         self.amount_of_tweets_to_gather = 101
         # We want the top 5 most occurring terms
-        self.top_x_terms = 25
+        self.top_x_terms = 23
         self.second_net_multiplier = 2
         # On a scale up to X.0, what is the scale that the term frequency document should follow
         self.numeric_scale = 10
@@ -29,10 +29,10 @@ class RecommenderTextual:
         # worth double the worth of a term)
         # This is currently bugged however, see bugs section above.
         self.hash_tag_multiplier = 2
-        self.single_tweet_feedback = single_tweet_feedback#{"me":"m"}
-        self.liked_tweets = []
-        self.disliked_tweets = []
-        self.liked_authors = []
+        self.single_tweet_feedback = single_tweet_feedback #{"me":"m"}#single_tweet_feedback["author"]
+        self.liked_tweets = []#single_tweet_feedback["like"]
+        self.disliked_tweets = []#single_tweet_feedback["dislike"]
+        self.liked_authors = []#single_tweet_feedback["like"]
         self.disliked_authors = []
         self.termfreq_doc = {}
         self.second_net_termfreq_doc = {}
@@ -41,13 +41,13 @@ class RecommenderTextual:
         ###################
         # Method calls, etc#
         ###################
-        #self.vectorizer = CountVectorizer()
         self.own_tweets = users_own_tweets
         self.followed_tweets = users_followed_tweets
         self.get_term_frequency_weightings()
         self.set_feedback()
         self.author_feedback = self.set_author_sentiment()
         self.set_tweet_feedback()
+        self.vectorizer = CountVectorizer()
 
     def set_tweet_feedback(self):
         for tweet_text in self.liked_tweets:
@@ -66,6 +66,14 @@ class RecommenderTextual:
             print(str(author) + " liked!")
             author_feedback[author] = author_feedback.get(author, 0) + 1
         return author_feedback
+
+    '''def contains_tweet_text(self, tlist, test_tweet):
+        contain = False
+        for tweet in tlist:
+            #if tweet['text'] == test_tweet['text']:
+            if tweet.text == test_tweet.text:
+                contain = True
+        return contain'''
 
     def set_feedback(self):
         #WORKS
@@ -167,8 +175,9 @@ class RecommenderTextual:
         # Removes the empty string from the term frequency document. This is a fix for an issue we were not able to
         # resolve otherwise. What happens is that terms that only contain special characters get aggregated together
         # to create a "super" term. This is not indented so we remove the empty string as a viable term.
-        print("This is the term frequency document")
-        print(self.termfreq_doc)
+        
+        #print("This is the term frequency document")
+        #print(self.termfreq_doc)
         if "" in self.termfreq_doc.keys():
             print("POPPED the empty string!")
             self.termfreq_doc.pop("")
@@ -190,9 +199,9 @@ class RecommenderTextual:
         return weighting
 
     def get_author_sentiment(self, tweet):
-        #WORKS
-        #author_name = tweet.author._json['screen_name']
         author_name = tweet['user']['screen_name']
+        #author_name = tweet.author.screen_name
+        print("scored")
         score = 0.0
         print(str(author_name.lower().encode("utf-8")))
         for name in self.author_feedback.keys():
@@ -201,11 +210,12 @@ class RecommenderTextual:
                 multiplier = self.more_or_less_from_this_author_multiplier
                 scale = self.numeric_scale
                 score = float(feedback_score)/float(scale) * float(multiplier)
+        print(score)
         return score
 
     def get_dislike_weighting(self, tweet):
         #WORKS
-        tweet_text = tweet['text']#.lower()
+        tweet_text = tweet#['text']#.lower()
         #tweet_text = tweet#.text.lower()
         print("tweet")
         print("DISLIKED " + str(tweet_text))
@@ -225,7 +235,6 @@ class RecommenderTextual:
             self.balance_reduce_term_freq_doc_preference(terms_to_reduce)
         
     def balance_reduce_term_freq_doc_preference(self, terms_to_reduce):
-        #WORKS
         num_of_reduced_terms = len(terms_to_reduce)
         num_of_terms = len(self.termfreq_doc.keys())
         alter_value = self.like_and_dislike_multiplier
@@ -246,7 +255,7 @@ class RecommenderTextual:
 
     def get_liked_weighting(self, tweet):
         #WORKS
-        tweet_text = tweet['text']#.lower()
+        tweet_text = tweet#['text']#.lower()
         #tweet_text = tweet#.text.lower()
         print("Tweet")
         print("LIKED" + str(tweet_text))
@@ -266,7 +275,6 @@ class RecommenderTextual:
             self.balance_increase_term_freq_doc_preference(terms_to_increase)
 
     def balance_increase_term_freq_doc_preference(self, terms_to_increase):
-        #WORKS
         num_of_increased_terms = len(terms_to_increase)
         num_of_terms = len(self.termfreq_doc.keys())
         alter_value = self.like_and_dislike_multiplier 
@@ -288,21 +296,26 @@ class RecommenderTextual:
 
     def generate(self, number_of_recommendations, how_many_days_ago):
         """What does this function do?"""
-
         #WORKS
-        list_of_owners_tweets = []
         max_age_in_seconds = how_many_days_ago * 86400  # number of seconds in 1 day
         seconds_ago = 0
         if how_many_days_ago > 7:
             max_age_in_seconds = 1000000
-        
-        for tweet in self.own_tweets:
-            #list_of_owners_tweets.append(tweet.text.encode('utf-8'))
-            list_of_owners_tweets.append(tweet['text'].encode('utf-8'))  # UNCOMMENT THIS LINE BEFORE COMMITTING AND COMMENT OUT LINE ABOVE
+
+        #for tweet in self.own_tweets:
+        #    list_of_owners_tweets.append(tweet.text.encode('utf-8'))
+            #list_of_owners_tweets.append(tweet['text'].encode('utf-8'))  # UNCOMMENT THIS LINE BEFORE COMMITTING AND COMMENT OUT LINE ABOVE
 
         #self.vectorizer.fit_transform(list_of_owners_tweets)
         words = self.own_tweets  # The users own tweets
         tweet_list = self.followed_tweets  # tweets from accounts that the user is following
+
+        '''tweet_list = []
+        for tweet in tweet_list_x:
+            if self.contains_tweet_text(tweet_list, tweet):
+                continue
+            else:
+                tweet_list.append(tweet)'''
 
         remove_these_tweets = []
 
@@ -313,8 +326,6 @@ class RecommenderTextual:
             tweet_age = datetime.strptime(tweet_age, '%a %b %d %H:%M:%S +0000 %Y')  # dirty fix
             age = time.time() - (tweet_age - datetime(1970, 1, 1)).total_seconds()
             if age > max_age_in_seconds:
-                print(tweet.text.encode("utf-8"))
-                print(tweet.created_at)
                 remove_these_tweets.append(tweet)
 
         for removal in remove_these_tweets:
@@ -379,8 +390,12 @@ class RecommenderTextual:
         # count!
         # 6
 
-        #Works with negative and positive
         count += self.get_author_sentiment(tweet)
+        if tweet in self.disliked_tweets:
+            self.get_dislike_weighting(tweet)
+
+        if tweet in self.liked_tweets:
+            self.get_liked_weighting(tweet)
 
         for word in sanitised_tweet_text.split():
             if word[0] == "#":
