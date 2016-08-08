85516e6dc04b46a84a12327d3fdbc924bcdf6330
Xinqi Li <xinqi.li@ucdconnect.ie>
2016-08-08 20:28:31 +0100

    Changed 'is' to '=='

--- backend/RecommenderTextual.py
+++ backend/RecommenderTextual.py
@@ -78,16 +78,16 @@ class RecommenderTextual:
     def set_feedback(self):
         #WORKS
         for feedback in self.single_tweet_feedback:
-            if feedback.get("feedback", None).lower() is "like" and feedback.get("reason", None).lower() is "subject":
+            if feedback.get("feedback", None).lower() == "like" and feedback.get("reason", None).lower() == "subject":
                 self.liked_tweets.append(str(feedback.get("tweetContent", None)))
 
-            elif feedback.get("feedback", None).lower() is "dislike" and feedback.get("reason", None).lower() is "subject":
+            elif feedback.get("feedback", None).lower() == "dislike" and feedback.get("reason", None).lower() == "subject":
                 self.disliked_tweets.append(str(feedback.get("tweetContent", None)))
             
-            elif feedback.get("feedback", None).lower() is "like" and feedback.get("reason", None).lower() is "author":
+            elif feedback.get("feedback", None).lower() == "like" and feedback.get("reason", None).lower() == "author":
                 self.liked_authors.append(str(feedback.get("followerScreenName", None)))
             
-            elif feedback.get("feedback", None).lower() is "dislike" and feedback.get("reason", None).lower() is "author":
+            elif feedback.get("feedback", None).lower() == "dislike" and feedback.get("reason", None).lower() == "author":
                 self.disliked_authors.append(str(feedback.get("followerScreenName", None)))
             
             else:
